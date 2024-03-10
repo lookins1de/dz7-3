ff2dd0149d690e02598725603ce54cb5c1135d11
Alexander <bl055@rambler.ru>
2024-03-11 00:30:10 +0300

    save1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿int[] array = CreateArray(10, 10, 0);
+
+
+
+static int[] CreateArray(int size, int max, int min)
+{
+    int[] array = new int[size];
+    Random rand = new();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(min, max + 1);
+    }
+    return array;
+}
+
+static string PrintArray(int[] array)
+{
+    return string.Join(", ", array);
+}
+
+static int[] ArraySwap(int[] array, int index)
+{
+
+    if (index <= 0)
+    {
+        return array;
+    }
+    int temp = array[index];
+    array[index] = array[array.Length - index - 1];
+    array[array.Length - index - 1] = temp;
+    return ArraySwap(array, index -= 2);
+}
+
+
+Console.WriteLine($"Произвольный массив => [{PrintArray(array)}]");
+Console.WriteLine($"Перевернутый массив => [{PrintArray(ArraySwap(array, array.Length - 1))}]");
+
